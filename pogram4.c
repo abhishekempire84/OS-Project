296a82baf061b9f68c2ccb79b9c4d2d75f98510f
abhishekempire84 <42838235+abhishekempire84@users.noreply.github.com>
2019-04-14 21:20:17 +0530

    Update pogram4.c

--- pogram4.c
+++ pogram4.c
@@ -1,44 +1,44 @@
 #include<stdio.h>
 int main()
 {
-	int a_time[10],b_time[10],temp[10];
-	int i,small,count=0,time,limit;
-	double w_time=0,t_around_time=0,terminate;
-	float avg_w_time,avg_t_around_time;
+	int at[10],bt[10],temp[10];
+	int i,small,count=0,time,n;
+	double wt=0,t_around_t=0,terminate;
+	float avg_w_t,avg_t_around_t;
 	printf("\nEnter the total number of processes:\n");
-	scanf("%d",&limit);
-	printf("\nEnter details of %d Processes\n",limit);
-	for(i=0;i<limit;i++)
+	scanf("%d",&n);
+	printf("\nEnter details of %d Processes\n",n);
+	for(i=0;i<n;i++)
 	{
 		printf("\nEnter Arrival Time:\t");
-		scanf("%d",&a_time[i]);
+		scanf("%d",&at[i]);
 		printf("Enter Burst Time:\t");
-		scanf("%d",&b_time[i]);
-		temp[i]=b_time[i];
+		scanf("%d",&bt[i]);
+		temp[i]=bt[i];
 	}
-b_time[9]=9999;
-for(time=0;count!=limit;time++)
+bt[9]=9999;
+for(time=0;count!=n;time++)
 {
 	small=9;
-	for(i=0;i<limit;i++)
+	for(i=0;i<n;i++)
 	{
-		if(a_time[i] <= time && b_time[i] < b_time[small] && b_time[i] > 0);
+		if(at[i] <= time && bt[i] < bt[small] && bt[i] > 0);
 		{
 			small=i;
 		}
 	}
-b_time[small]--;
-if(b_time[small]==0)
+bt[small]--;
+if(bt[small]==0)
 {
 	count++;
 	terminate=time + 1;
-	w_time=w_time + terminate - a_time[small] - temp[small];
-	t_around_time=t_around_time + terminate - a_time[small];
+	wt=w_time + terminate - at[small] - temp[small];
+	t_around_t=t_around_t + terminate - at[small];
 }
 }
-avg_w_time = w_time / limit;
-avg_t_around_time = t_around_time / limit;
-printf("\nAverage Waiting Time:\t%lf\n",avg_w_time);
-printf("\nAverage Turnaround Time:\t%lf\n",avg_t_around_time);
+avg_w_t = wt / n;
+avg_t_around_t = t_around_t / n;
+printf("\nAverage Waiting Time:\t%lf\n",avg_w_t);
+printf("\nAverage Turnaround Time:\t%lf\n",avg_t_around_t);
 return 0;
 }
